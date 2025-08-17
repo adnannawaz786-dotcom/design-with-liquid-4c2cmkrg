efd0c63d7a8d9cced19592df5ee91e1ceef03a69
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-17 04:31:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
-import Dashboard from './components/Dashboard';
+import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
